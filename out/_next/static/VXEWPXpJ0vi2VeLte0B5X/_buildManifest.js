ba3a17f750b5e364aae51e146842539cd557c839
WAYNE CHOI <wonjun92@gmail.com>
2021-08-04 06:44:22 +1200

    add out dir

---
+++ out/_next/static/VXEWPXpJ0vi2VeLte0B5X/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-738f60e287404c8647a7.js"],"/_error":["static/chunks/pages/_error-9faf4177fb4e528b4124.js"],"/blog/[slug]":["static/chunks/pages/blog/[slug]-17efcdca685563e532d2.js"],sortedPages:["/","/_app","/_error","/blog/[slug]"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
