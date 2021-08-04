b7cee1d22e189c8879170a4cfb596aee4d9750f5
WAYNE CHOI <wonjun92@gmail.com>
2021-08-05 05:57:03 +1200

    'deploy'

---
+++ out/_next/static/O_otEUm4bsv9XOOUttGfI/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-4bebd47477cb4eb8c9cd.js"],"/_error":["static/chunks/pages/_error-9faf4177fb4e528b4124.js"],"/blog/[slug]":["static/chunks/pages/blog/[slug]-17efcdca685563e532d2.js"],sortedPages:["/","/_app","/_error","/blog/[slug]"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
