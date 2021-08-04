fac87a50d80c577b5f55f88319e925816fbef977
WAYNE CHOI <wonjun92@gmail.com>
2021-08-05 05:38:11 +1200

    set up github page

--- next.config.js
+++ next.config.js
@@ -1,3 +1,7 @@
 module.exports = {
   reactStrictMode: true,
-}
+  assetPrefix:
+    process.env.NODE_ENV === "production"
+      ? "https://boramyy.github.io/invitation"
+      : "",
+};
--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,14 @@
 import "../styles/globals.css";
 import Header from "../components/header";
+import { PortfolioProvider } from "../context/context";
+import { prefix } from "../config/config";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <>
+    <PortfolioProvider value={{ prefix }}>
       <Header />
       <Component {...pageProps} />
-    </>
+    </PortfolioProvider>
   );
 }
 
