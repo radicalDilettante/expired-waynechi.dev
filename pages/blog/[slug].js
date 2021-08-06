7175d2c3eedb4f2e11424e052bacc91eacec4023
WAYNE CHOI <wonjun92@gmail.com>
2021-08-07 06:56:02 +1200

    markup blog page

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -1,5 +1,56 @@
 import React from "react";
+import fs from "fs";
+import path from "path";
+import matter from "gray-matter";
 
-export default function Blog() {
-  return <div></div>;
+import styles from "./blog.module.css";
+
+export default function Blog({
+  frontMatter: { title, date, cover_image },
+  content,
+}) {
+  let markedContent;
+  markedContent = content.split("\r\n");
+  markedContent = markedContent.filter((element, i) => element !== "");
+  return (
+    <div>
+      <h1>{title}</h1>
+      <p>{date}</p>
+      {cover_image && <img src={cover_image} className={styles.img} />}
+      {markedContent.map((text, index) => (
+        <p key={index}>{text}</p>
+      ))}
+    </div>
+  );
+}
+
+export async function getStaticPaths() {
+  const files = fs.readdirSync(path.join("posts"));
+
+  const paths = files.map((filename) => ({
+    params: {
+      slug: filename.replace(".md", ""),
+    },
+  }));
+
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+export async function getStaticProps({ params: { slug } }) {
+  const markdownWithMeta = fs.readFileSync(
+    path.join("posts", slug + ".md"),
+    "utf-8"
+  );
+
+  const { data: frontMatter, content } = matter(markdownWithMeta);
+
+  return {
+    props: {
+      frontMatter,
+      content,
+    },
+  };
 }
