09d0302665c87f3b3a031a3a1e0bda1a0e015bb8
WAYNE CHOI <wonjun92@gmail.com>
2021-08-08 16:44:33 +1200

    remove three.js, and set 2d canvas // don't need 3d animation but 2d

--- components/home/animation.jsx
+++ components/home/animation.jsx
@@ -1,73 +1,28 @@
 import React, { useEffect, useRef, useState, useCallback } from "react";
-import {
-  BoxGeometry,
-  Mesh,
-  PerspectiveCamera,
-  Scene,
-  WebGLRenderer,
-  DirectionalLight,
-  MeshPhongMaterial,
-  Color,
-} from "three";
 
 import styles from "./animation.module.css";
 
 export default function Animation() {
-  const canvas = useRef(null);
+  const container = useRef(null);
 
   useEffect(() => {
-    const renderer = new WebGLRenderer();
-    canvas.current.appendChild(renderer.domElement);
-    const width = canvas.current.offsetWidth;
-    const height = (width * 2) / 3;
-    renderer.setSize(width, height);
+    const canvas = document.createElement("canvas");
+    const ctx = canvas.getContext("2d");
+    container.current.appendChild(canvas);
 
-    const fov = 75; //filed of view
-    const aspect = 2; //default ratio
-    const near = 0.1;
-    const far = 5;
-    const camera = new PerspectiveCamera(fov, aspect, near, far);
+    const stageWidth = container.current.offsetWidth;
+    const stageHeight = (stageWidth * 2) / 3;
 
-    camera.position.z = 2;
+    const canvasWidth = stageWidth * 2;
+    const canvasHeight = stageHeight * 2;
+    ctx.scale(2, 2);
 
-    const scene = new Scene();
-    scene.background = new Color(0xffffff);
-
-    //add light
-    const color = 0xffffff;
-    const intensity = 1;
-    const light = new DirectionalLight(color, intensity);
-    light.position.set(-1, 2, 4);
-    scene.add(light);
-
-    //geometry
-    const boxWidth = 1;
-    const boxHeight = 1;
-    const boxDepth = 1;
-    const geometry = new BoxGeometry(boxWidth, boxHeight, boxDepth);
-
-    //const material = new MeshBasicMaterial({ color: 0x44aa88 });
-    //make a material for shiny surfaces with specular highlights.
-    const material = new MeshPhongMaterial({ color: 0x44aa88 });
-
-    const cube = new Mesh(geometry, material);
-
-    scene.add(cube);
-    function render(time) {
-      time *= 0.001; // convert time to seconds
-
-      const canvas = renderer.domElement;
-      camera.aspect = canvas.clientWidth / canvas.clientHeight;
-      camera.updateProjectionMatrix();
-
-      cube.rotation.x = time;
-      cube.rotation.y = time;
-
-      renderer.render(scene, camera);
-
+    const render = () => {
       requestAnimationFrame(render);
-    }
+
+      ctx.clearRect(0, 0, stageWidth, stageHeight);
+    };
     requestAnimationFrame(render);
   }, []);
-  return <div ref={canvas} className={styles.container}></div>;
+  return <div ref={container} className={styles.container}></div>;
 }
