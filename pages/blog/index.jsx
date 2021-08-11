c98d222e1e2b4bda21e1fdd1cdf650134c2ada93
WAYNE CHOI <wonjun92@gmail.com>
2021-08-12 05:31:24 +1200

    modulise blog list service

--- pages/blog/index.jsx
+++ pages/blog/index.jsx
@@ -1,13 +1,9 @@
 import React from "react";
 
-import fs from "fs";
-import path from "path";
-import matter from "gray-matter";
-
 import styles from "./index.module.css";
 
 import PostCard from "../../components/blog/post_card";
-import sortByDate from "../../service/sort_by_date";
+import blogList from "../../service/blog/blog_list";
 
 export default function index({ posts, prefix }) {
   return (
@@ -22,28 +18,9 @@ export default function index({ posts, prefix }) {
 }
 
 export async function getStaticProps() {
-  const files = fs.readdirSync(path.join("posts"));
-
-  const posts = files.map((filename) => {
-    const slug = filename.replace(".md", "");
-
-    const markdownWithMeta = fs.readFileSync(
-      path.join("posts", filename),
-      "utf-8"
-    );
-
-    const { data: frontMatter, content } = matter(markdownWithMeta);
-
-    return {
-      slug,
-      content,
-      frontMatter,
-    };
-  });
-
   return {
     props: {
-      posts: posts.sort(sortByDate),
+      posts: blogList(),
     },
   };
 }
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,12 +1,9 @@
 import React, { useEffect, useRef } from "react";
 import styles from "./index.module.css";
 import { Main } from "../service/animation/main";
-import sortByDate from "../service/sort_by_date";
 
 import PostCard from "../components/blog/post_card";
-import fs from "fs";
-import path from "path";
-import matter from "gray-matter";
+import blogList from "../service/blog/blog_list";
 
 export default function Home({ prefix, posts }) {
   const canvas = useRef();
@@ -32,27 +29,13 @@ export default function Home({ prefix, posts }) {
 }
 
 export async function getStaticProps() {
-  const files = fs.readdirSync(path.join("posts"));
-
-  const posts = files.map((filename) => {
-    const markdownWithMeta = fs.readFileSync(
-      path.join("posts", filename),
-      "utf-8"
-    );
-
-    const { data: frontMatter } = matter(markdownWithMeta);
-
-    return {
-      frontMatter,
-    };
-  });
-  let result = posts.sort(sortByDate);
+  const posts = blogList();
+  let result = posts;
   if (result.length >= 5) {
     result = result.slice(0, 4);
   }
   return {
     props: {
-      files,
       posts: result,
     },
   };
