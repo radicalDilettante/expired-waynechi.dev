7b1afb0d586a229937a5fd7a69c12c39e84e7ccc
WAYNE CHOI <wonjun92@gmail.com>
2021-09-28 20:32:53 +1300

    change code block style

--- components/blog/markdown_render.jsx
+++ components/blog/markdown_render.jsx
@@ -1,15 +1,15 @@
 /* eslint-disable react/no-children-prop */
 import React, { useRef, useEffect } from "react";
 import Markdown from "markdown-to-jsx";
-import hljs from "highlight.js";
-import "highlight.js/styles/atom-one-dark.css";
+import highlightJs from "highlight.js";
+import "highlight.js/styles/a11y-light.css";
 
 export default function MarkdownRender({ content, prefix }) {
   const rootRef = useRef();
 
   useEffect(() => {
     rootRef.current.querySelectorAll("pre code").forEach((block) => {
-      hljs.highlightBlock(block);
+      highlightJs.highlightBlock(block);
     });
   }, [content]);
   return (
