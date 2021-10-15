4ec381e3a3c2c89366fc37ba9510cf200a7563c8
WAYNE CHOI <wonjun92@gmail.com>
2021-10-16 00:18:54 +1300

    change library to parse markdown

--- components/blog/markdown_render.tsx
+++ components/blog/markdown_render.tsx
@@ -3,6 +3,7 @@ import React, { useRef, useEffect } from "react";
 import Markdown from "markdown-to-jsx";
 import highlightJs from "highlight.js";
 import "highlight.js/styles/a11y-dark.css";
+import marked from "marked";
 
 interface IProps {
   content: string;
@@ -13,17 +14,15 @@ export default function MarkdownRender({ content, prefix }: IProps) {
   const rootRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
-    if (rootRef.current) {
-      rootRef.current
-        .querySelectorAll<HTMLElement>("pre code")
-        .forEach((block) => {
-          highlightJs.highlightElement(block);
-        });
-    }
+    const parsedMarkdown = document.createElement("div");
+    parsedMarkdown.innerHTML = marked(content);
+    rootRef.current!.appendChild(parsedMarkdown);
+
+    rootRef
+      .current!.querySelectorAll<HTMLElement>("pre code")
+      .forEach((block) => {
+        highlightJs.highlightElement(block);
+      });
   }, [content]);
-  return (
-    <div ref={rootRef}>
-      <Markdown>{content.replace("images/", `${prefix}images/`)}</Markdown>
-    </div>
-  );
+  return <div ref={rootRef}></div>;
 }
