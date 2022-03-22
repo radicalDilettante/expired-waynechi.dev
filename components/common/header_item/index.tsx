fe0178105c3b9ec4546ca55c26731406cea22705
waynethebb <wonjun92@gmail.com>
2022-03-22 23:54:39 +1300

    debug a tag color issue

--- components/common/header_item/index.tsx
+++ components/common/header_item/index.tsx
@@ -1,25 +1,17 @@
 import React from "react";
 import Link from "next/link";
 
-import styles from "./style.module.css";
-import { useRouter } from "next/router";
-
 interface IProps {
   menu: { name: string; path: string };
 }
 
 export default function HeaderItem({ menu }: IProps) {
-  const router = useRouter();
   return (
-    <li
-      className={
-        router.pathname === menu.path
-          ? styles.list_item_active
-          : styles.list_item
-      }
-    >
-      <div className={styles.link}>
-        <Link href={menu.path}>{menu.name}</Link>
+    <li>
+      <div>
+        <Link href={menu.path}>
+          <a href={menu.path}>{menu.name}</a>
+        </Link>
       </div>
     </li>
   );
