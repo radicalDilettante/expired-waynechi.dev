00b86e37b52b64bc097112c686286a51c4fe9881
waynethebb <wonjun92@gmail.com>
2022-03-24 23:04:42 +1300

    change cv page

--- pages/cv.tsx
+++ pages/cv.tsx
@@ -47,17 +47,18 @@ export default function CV() {
           sitting over the past one year.
         </p>
         <p>
+          I value background more than tools. Of course, I've studied the deep
+          concepts of JavaScript, but I'm more interested in how the browser,
+          web and network work. I understand how HTTP and backend applications
+          work in order to create a better frontend applications. I try to use
+          semantic HTML5 tags to consider accesibility and SEO.
+        </p>
+        <p>
           I prefer readable code, and maintainable system. For those, I always
           try to separate implementations and interfaces. Recently, I have been
           struggling with the effecient way to modularize along with various
           dependencies from web frontend, like DOM, user interaction, and
           rendering libraries.
-        </p>
-        <p>
-          I value background more than tools. Of course, I've studied the deep
-          concepts of JavaScript, but I'm more interested in how the browser,
-          web and network work. I'm also interested in studying lower levels,
-          but right now, I'm trying to focus more on the web.
         </p>
       </div>
 
@@ -78,25 +79,25 @@ export default function CV() {
               https://github.com/radicalDilettante/skilled_kiwi_client
             </a>
             <ul>
-              <li>React Application with Next.js.</li>
+              <li>React Application with Next.js</li>
               <li>
                 Reusable custom hooks with branching process against a variety
-                of conditions.
+                of conditions
               </li>
               <li>
                 Context API to mange global statement, and SWR to cache Ajax
-                response data.
+                response data
               </li>
               <li>
                 Authentication with JWT token. It stored in local storage.
               </li>
               <li>
                 User input data are filltered and sanitzied before Ajax request
-                with them.
+                with them
               </li>
               <li>
                 Unit test with Jest, and all modules are decopuled reasonably
-                for the test.
+                for the test
               </li>
             </ul>
             <b>Backend</b>:{" "}
@@ -107,15 +108,10 @@ export default function CV() {
               https://github.com/radicalDilettante/skilled_kiwi_client
             </a>
             <ul>
-              <li>Express application with MySQL database.</li>
-              <li>
-                While I am focusing on frontend more, I just implemented rest
-                API endpoint just every time I need in a bottom-up approach.
-              </li>
-              <li>
-                So I cannot guarantee quality of the code. I might build whole
-                new application later on with other framework.
-              </li>
+              <li>Express application with MySQL database</li>
+              <li>RESTful api for CRUD operations</li>
+              <li>Authentication with JWT tokens (access and refresh)</li>
+              <li>Documentation the API endpoints and requirements</li>
             </ul>
           </div>
         </div>
@@ -132,8 +128,8 @@ export default function CV() {
             </a>
             <ul>
               <li>Static blog site generated with Next.js</li>
-              <li>Posts are written by markdown, and parsed to JSX.</li>
-              <li>Animation with canvas 2d APIs.</li>
+              <li>Build a deployment automation with github action</li>
+              <li>Create an animation with canvas APIs</li>
               <li>Implement command line rendering for blog contents</li>
             </ul>
           </div>
@@ -182,6 +178,18 @@ export default function CV() {
             <li>Carbon super yacht building, Restoration sailing boats</li>
             <li>Laminating, Trouble shooting, Assembly, Detailing</li>
           </ul>
+        </div>
+      </div>
+
+      <div>
+        <h2>Education</h2>
+        <div className={styles.education}>
+          <b>Manukau Institute of Technology</b>
+          <span> - Diploma, Computer Integrated Manufacturing</span>
+        </div>
+        <div className={styles.education}>
+          <b>Unitec Institute of Technology</b>
+          <span> - Certificate in Applied Technology in Boatbuilding</span>
         </div>
       </div>
     </div>
